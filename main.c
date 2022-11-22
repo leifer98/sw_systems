8e7afd04cb8a4a5b2d08731461f1407c7cf14002
leifer <ofriofriofri@gmail.com>
2022-11-22 17:24:15 +0200

    10 commit

--- main.c
+++ main.c
@@ -3,47 +3,11 @@
 
 int main()
 {
-	int num1, num2;
-	//printf("Enter your first number: ");
-	scanf("%d", &num1);
-	//printf("\nEnter your second number: ");
-	scanf("%d", &num2);
+	int mat[4][4] = {	{0,1,0,4},
+						{1,0,1,0},
+						{0,1,0,1},
+						{4,0,1,0}};
 	printf("The Armstrong numbers are:");
-	for(int i=num1; i<=num2; i++)
-	{
-		int result = isArmstrong(i);
-		if (result==1)
-		{
-			printf(" %d", i);
-		}
-	}
-	printf("\nThe Palindromes are:");
-	for(int i=num1; i<=num2; i++)
-	{
-		int result = isPalindrome(i);
-		if (result==1)
-		{
-			printf(" %d", i);
-		}
-	}
-	printf("\nThe Prime numbers are:");
-	for(int i=num1; i<=num2; i++)
-	{
-		int result = isPrime(i);
-		if (result==1)
-		{
-			printf(" %d", i);
-		}
-	}
-	printf("\nThe Strong numbers are:");
-	for(int i=num1; i<=num2; i++)
-	{
-		int result = isStrong(i);
-		if (result==1)
-		{
-			printf(" %d", i);
-		}
-	}
-	printf("\n");
+
 	return 0;
 }
