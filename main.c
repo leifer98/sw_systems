355810eb4bf3719ea92dfa44474993bec7e4c368
matro68 <ofriofriofri@gmail.com>
2023-01-17 11:59:02 +0200

    fixed some bugs

--- main.c
+++ main.c
@@ -4,7 +4,9 @@
 
 int main()
 {
-	pnode *head = (pnode *)malloc(sizeof(node));
+	// pnode *head = (pnode *)malloc(sizeof(node));
+	pnode *head = (pnode *)calloc(1, sizeof(node));
+
 	int check;
 	char c; // c the input from the user, ch1 for whitespace chars we will get in adition
 	check = scanf(" %c", &c);
@@ -15,7 +17,8 @@ int main()
 			break;
 		if (c == 'A')
 		{
-			deleteGraph_cmd(head);
+			if (*(head) != NULL)
+				deleteGraph_cmd(head);
 			build_graph_cmd(head);
 			scanf(" %c", &c);
 			while (c == 'n')
@@ -37,57 +40,29 @@ int main()
 			scanf(" %d", &i);
 			delete_node_cmd(head, i);
 			check = scanf(" %c", &c);
-			if (check != 1) break;
+			if (check != 1)
+				break;
 		}
 		else if (c == 'T')
 		{
+			if (*(head) != NULL)
+				break;
 			TSP_cmd(*head);
 			check = scanf(" %c", &c);
-			if (check != 1) break;
+			if (check != 1)
+				break;
 		}
 		else if (c == 'S')
 		{
+			if (*(head) != NULL)
+				break;
 			shortsPath_cmd(*head);
 			check = scanf(" %c", &c);
-			if (check != 1) break;
+			if (check != 1)
+				break;
 		}
 	}
 	deleteGraph_cmd(head);
 	free(head);
 	return 0;
 }
-// if (c == 'A')
-// {
-// 	build_graph_cmd(head);
-// 	scanf(" %c", &c);
-// 	// printf("14. got char from input: %c\n", c);
-// 	while (c == 'n')
-// 	{
-// 		c = insert_node_cmd(head);
-// 	}
-// 	// printGraph_cmd(*head);
-// }
-// if (c == 'B')
-// {
-// 	c = insert_node_cmd(head);
-// 	// printGraph_cmd(*head);
-// }
-// if (c == 'T')
-// {
-// 	TSP_cmd(*head);
-// 	scanf(" %c", &c);
-// }
-// if (c == 'S')
-// {
-// 	shortsPath_cmd(*head);
-// 	scanf(" %c", &c);
-
-// 	// printf("20. got char from input: %c\n", c);
-// }
-// printGraph_cmd(*head);
-// delete_node_cmd(head, 2);
-// printGraph_cmd(*head);
-
-// // freeing all nodes
-// deleteGraph_cmd(head);
-// printGraph_cmd(*head);
